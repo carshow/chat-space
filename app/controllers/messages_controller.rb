ae270d05f5c47a5de0ee150da9d995f7d9715845
carshow <saladvar1212@gmail.com>
2018-02-10 14:49:03 +0900

    Set_@group before_action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,11 @@
 class MessagesController < ApplicationController
+before_action :set_groups
   def index
+  end
+
+  private
+
+  def set_groups
+    @group = Group.find(params[:group_id])
   end
 end
