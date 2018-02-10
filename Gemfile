0d5440cd47201432c9c19c0ab64c5d76c5b47de1
carshow <saladvar1212@gmail.com>
2018-02-10 14:16:58 +0900

    Install gem[carrierwave,mini_magick]

--- Gemfile
+++ Gemfile
@@ -60,4 +60,5 @@ gem 'font-awesome-rails'
 gem 'devise'
 gem 'haml-rails'
 gem 'erb2haml'
-
+gem 'carrierwave'
+gem 'mini_magick'
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,5 @@ class Message < ApplicationRecord
   validates :content, presence:true, unless :image?
   belongs_to :group
   belongs_to :user
+   mount_uploader :image, ImageUploader
 end
