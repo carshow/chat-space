d3f48d6a8df12a0d22767f41f8d73760a2fef6db
carshow <saladvar1212@gmail.com>
2018-02-24 19:43:59 +0900

    Create method in model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,18 @@
 class Message < ApplicationRecord
-  validates :body, presence: true, unless: :image?
   belongs_to :group
   belongs_to :user
   mount_uploader :image, ImageUploader
+  validates :body, presence: true, unless: :image?
+
+   def show_last_message
+    if (last_message = messages.last).present?
+      if last_message.body?
+       last_message.body
+      else
+        '画像が投稿されています'
+      end
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
