674156517398cc5beec20d935ce14c35f17a134c
carshow <saladvar1212@gmail.com>
2018-02-24 19:10:05 +0900

    Patch text in message_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,11 +9,10 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      flash[:notice] = "メッセージが送信されました。"
-      redirect_to group_messages_path(@group)
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
       @messages = @group.messages.includes(:user)
-      flash.now[:alert] = "メッセージを入力ください。"
+      flash.now[:alert] = 'メッセージを入力してください。'
       render :index
     end
   end
