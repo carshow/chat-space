e2610ea13b47570ff8d01b278f1dd9993e6bbb26
carshow <saladvar1212@gmail.com>
2018-03-10 19:01:57 +0900

    Patch messages-controller to change to ajax

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,23 +4,26 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+    @users = @group.users
   end
 
   def create
     @message = @group.messages.new(message_params)
-    if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
-    else
-      @messages = @group.messages.includes(:user)
-      flash.now[:alert] = 'メッセージを入力してください。'
-      render :index
+    respond_to do |format|
+      if @message.save
+        format.html { redirect_to group_messages_path(@group) }
+        format.json
+      else
+        format.html { render :index}
+      end
     end
+
   end
 
   private
 
   def message_params
-    params.require(:message).permit(:body, :image).merge(user_id: current_user.id)
+    params.require(:message).permit(:body, :image, :created_at).merge(user_id: current_user.id)
   end
 
   def set_group
