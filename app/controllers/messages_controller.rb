fe89f77350352c9b133aaf5bbd73780234220373
carshow <saladvar1212@gmail.com>
2018-03-11 19:59:49 +0900

    Modify messages-controller for Ajax auto reloading

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,10 @@ class MessagesController < ApplicationController
     @message = Message.new
     @messages = @group.messages.includes(:user)
     @users = @group.users
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def create
